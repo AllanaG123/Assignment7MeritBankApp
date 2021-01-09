ad4b793f6decc63d9c2f0bda0441c37917eeeecb
Allana <allanagray10@gmail.com>
2021-01-09 13:43:34 -0600

    Update AuthenticationRequest.java

--- src/main/java/com/assignments/assignment7/Security/AuthenticationRequest.java
+++ src/main/java/com/assignments/assignment7/Security/AuthenticationRequest.java
@@ -8,7 +8,28 @@ public class AuthenticationRequest {
 	public AuthenticationRequest() {
 		
 	}
-	public AuthenticatonRequest (String username, String )
+	public  AuthenticationRequest (String username, String password) {
+		this.username = username;
+		this.password = password;
+		
+	}
 	
+	public String getUsername() {
+		return username;
+		
+	}
+	public void setUsername (String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+		
+	}
+	
+	public void setPassword (String password) {
+		this.password = password;
+		
+	}
 
 }
